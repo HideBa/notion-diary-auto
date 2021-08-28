6b0aaccec2f0fec68f24aed12c99699958ac1b37
HideBa <baba.papa1120.ba@gmail.com>
2021-08-28 11:56:26 +0900

    impl getter of diary

--- domain/diary.go
+++ domain/diary.go
@@ -1,11 +1,11 @@
 package domain
 
 type Diary struct {
-	name    string
-	author  string
-	news    *News
-	calenar *Calendar
-	weather *Weather
+	name     string
+	author   string
+	news     *News
+	calendar *Calendar
+	weather  *Weather
 }
 
 func NewDiary(n string, a string) *Diary {
@@ -13,4 +13,24 @@ func NewDiary(n string, a string) *Diary {
 		name:   n,
 		author: a,
 	}
+}
+
+func (d *Diary) Name() string {
+	return d.name
+}
+
+func (d *Diary) Author() string {
+	return d.author
+}
+
+func (d *Diary) News() *News {
+	return d.news
+}
+
+func (d *Diary) Calendar() *Calendar {
+	return d.calendar
+}
+
+func (d *Diary) Weather() *Weather {
+	return d.weather
 }
