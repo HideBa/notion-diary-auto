fb4ce3d0196fd586ae2ccfc5d5dbe441fc50f829
HideBa <baba.papa1120.ba@gmail.com>
2021-08-31 09:52:52 +0900

    fix route

--- infrastructure/router/router.go
+++ infrastructure/router/router.go
@@ -9,6 +9,6 @@ func Api(r *echo.Group, c *controller.Controller) {
 	r.GET("/ping", func(c echo.Context) error {
 		return c.JSON(200, "pong")
 	})
-	r.GET("/diary", c.DiaryController.Fetch)
-	r.POST("/diary", c.DiaryController.Create)
+	r.GET("/diaries", c.DiaryController.Fetch)
+	r.POST("/diaries", c.DiaryController.Create)
 }
